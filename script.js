0418a271cf21b9b461113943f1cc302613abaa2a
alexfortin <fortin.alex14@gmail.com>
2022-09-23 17:47:33 -0500

    made the movie selections more clean, and made event lister to listen when they get clicked

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ var displaySearch = function () {
                 var movieTitle = (data.Search[i].Title)
                 var movieYear = (data.Search[i].Year)
                 var moviePoster = (data.Search[i].Poster)
+                var imdbId = (data.Search[i].imdbId)
                 movieDropDown(movieTitle, movieYear, moviePoster)
             }
 
@@ -26,14 +27,33 @@ var displaySearch = function () {
 var movieDropDown = function(movieTitle, movieYear, moviePoster) {
     
     var movieBox = document.createElement("li")
+    movieBox.className = "display-movie-box"
+    var titleParagraph = document.createElement("p")
+    var yearParagraph = document.createElement("p")
     var posterImage = document.createElement("img")
     posterImage.setAttribute("src", moviePoster)
+    posterImage.className = "form-movie-poster"
 
-    $(movieBox).append(movieTitle)
-    $(movieBox).append(movieYear)
+    titleParagraph.innerText = movieTitle
+    titleParagraph.className = "form-movie-title"
+    yearParagraph.innerText = movieYear
+    yearParagraph.className = "form-movie-year"
+
+    $(movieBox).append(titleParagraph)
+    $(movieBox).append(yearParagraph)
     $(movieBox).append(posterImage)
     $(movieSelectionContainer).append(movieBox)
     console.log(posterImage)
 }
 
+var displayMovieInfo = function() {
+
+
+
+}
+
 $(inputBox).on("keyup", displaySearch)
+
+$(movieSelectionContainer).on("click", ".display-movie-box", function() {
+    console.dir(this)
+} )
