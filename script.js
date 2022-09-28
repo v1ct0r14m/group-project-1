79b32dfb767afddf14f759a0d6bf10d3ec8f3c1f
alexfortin <fortin.alex14@gmail.com>
2022-09-28 16:47:05 -0500

    after working and debbuging mos code the rows poster is clear and the clicking movie function works and added practice styles

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var inputBox = $("#input-box")
 console.log(inputBox)
 
 //searches for the movie whenever you tyoe
-var searchMovie = function () {
+var searchMovie = function (event) {
+    event.preventDefault(event);
     //input goes into "movie" variable
     var movie = $(inputBox).val().trim()
     console.log(movie)
@@ -14,15 +15,17 @@ var searchMovie = function () {
     fetch(apiUrl).then(function (response) {
         response.json().then(function (data) {
             movieSelectionContainer.empty()
+            console.log(data)
             //for the lenght movie search results it will asign the values
             for (var i = 0; i < data.Search.length; i++) {
                 console.log(data)
                 var movieTitle = (data.Search[i].Title)
                 var movieYear = (data.Search[i].Year)
                 var moviePoster = (data.Search[i].Poster)
-                var imdbId = (data.Search[i].imdbId)
+                var imdbId = (data.Search[i].imdbID)
                 //calls the plot function and stores the asigned values to be used later
                 callPlot(imdbId, movieTitle, movieYear, moviePoster)
+                callProvider(imdbId)
             }
 
         })
@@ -45,42 +48,68 @@ var callPlot = function (imdbId, movieTitle, movieYear, moviePoster) {
     })
 }
 
+var callProvider = function(imdbId) {
+
+    var providerUrl = "https://api.watchmode.com/v1/title/" + imdbId + "/sources/?apiKey=nVHLIwU5mAmXtOJwbzoUbOnW6RaM85Y39M8Otx6L"
+    
+    fetch(providerUrl).then(function(response) {
+        response.json().then(function(data){
+            console.log(data)
+        })
+    })
+}
+
 var movieDropDown = function (movieTitle, movieYear, moviePoster, movieGenre, moviePlot) {
     //creates box to store all the elemnts
-    var movieBox = document.createElement("li")
-    movieBox.className = "display-movie-box"
+    var movieCard = document.createElement("div")
+    movieCard.className = "display-movie-box card"
 
-    //creates the elements where the values will be stored
+    //card Image
+    var cardImage = document.createElement("div")
+    cardImage.className = "card-image"
+    var figureImage = document.createElement('figure')
+    figureImage.className = "image is-3by4"
     var posterImage = document.createElement("img")
+    posterImage.className = "form-movie-poster"
     posterImage.setAttribute("src", moviePoster)
+
+
+    // card content 
+    var contentCard = document.createElement('div');
+    contentCard.className = 'card-content';
+    var conctent = document.createElement('div')
+    conctent.className = 'content'
     var titleParagraph = document.createElement("p")
     var yearParagraph = document.createElement("p")
     var genreParagraph = document.createElement("p")
     var plotParagraph = document.createElement("p")
 
-    //sets the values of the elements
-    posterImage.classList = "form-movie-poster hide"
     titleParagraph.innerText = movieTitle
-    titleParagraph.classList = "form-movie-title hide"
+    titleParagraph.classList = "form-movie-title"
     yearParagraph.innerText = movieYear
-    yearParagraph.classList = "form-movie-year hide"
+    yearParagraph.classList = "form-movie-year"
     genreParagraph.innerText = movieGenre
     genreParagraph.classList = "form-movie-genre hide"
     plotParagraph.innerText = moviePlot
     plotParagraph.classList = "form-movie-plot hide"
 
-    //appends all the elements to the box that stores the elemtns aka to the li
-    $(movieBox).append(posterImage)
-    $(movieBox).append(titleParagraph)
-    $(movieBox).append(yearParagraph)
-    $(movieBox).append(genreParagraph)
-    $(movieBox).append(plotParagraph)
-    //appends the li to ul in html
-    $(movieSelectionContainer).append(movieBox)
-}
+    //all appends are here
+    $(figureImage).append(posterImage)
+    $(cardImage).append(figureImage)
+    $(movieCard).append(cardImage)
+    $(conctent).append(titleParagraph)
+    $(conctent).append(yearParagraph)
+    $(conctent).append(genreParagraph)
+    $(conctent).append(plotParagraph)
+    $(contentCard).append(conctent)
+    $(movieCard).append(contentCard);
+    $(movieSelectionContainer).append(movieCard)
 
-//whenever you type it will call the search movie function
-$(inputBox).on("keyup", searchMovie)
+    //creates the elements where the values will be stored
+    var posterImage = document.createElement("img")
+    posterImage.setAttribute("src", moviePoster)
+}
+$("#search-button").on("click", searchMovie)
 
 //when you click on the list of movies it will display that movie and all of its information
 $(movieSelectionContainer).on("click", ".display-movie-box", function (event) {
@@ -91,21 +120,29 @@ $(movieSelectionContainer).on("click", ".display-movie-box", function (event) {
     var movieTitle = document.querySelector("#movie-title")
     var movieYear = document.querySelector("#movie-year")
     var movieGenre = document.querySelector("#genre")
+
     var moviePlot = document.querySelector("#plot")
 
     //creates the element where the values will be stored
     var poster = document.createElement("img")
+    poster.className = "stylePoster"
     var title = document.createElement("h2")
+    title.className = "styleTitle"
     var year = document.createElement("p")
+    year.className = "styleYear"
     var genre = document.createElement("p")
+    genre.className = "styleGenre"
     var plot = document.createElement("p")
+    plot.className = "stylePlot"
 
     //sets the values of the elements
-    poster.setAttribute("src", this.children[0].currentSrc)
-    title.innerText = this.children[1].innerText
-    year.innerText = this.children[2].innerText
-    genre.innerText = this.children[3].innerText
-    plot.innerText = this.children[4].innerText
+    poster.setAttribute("src", this.children[0].children[0].children[0].currentSrc)
+    title.innerText = this.children[1].children[0].children[0].innerText
+    year.innerText = this.children[1].children[0].children[1].innerText
+    genre.innerText = this.children[1].children[0].children[2].innerText
+    genre.classList.remove("hide")
+    plot.innerText = this.children[1].children[0].children[3].innerText
+    plot.classList.remove("hide")
 
     //appends elements to their containers
     $(moviePoster).empty().append(poster)
@@ -113,4 +150,11 @@ $(movieSelectionContainer).on("click", ".display-movie-box", function (event) {
     $(movieYear).empty().append(year)
     $(movieGenre).empty().append(genre)
     $(moviePlot).empty().append(plot)
+
+    if(plot.classList !== "hide") {
+        movieSelectionContainer.className = ("hide")
+        $(movieSelectionContainer).empty().append(movieSelectionContainer)
+    }
+
+    console.log("plot and genre work")
 })
