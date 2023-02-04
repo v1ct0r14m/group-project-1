5810ac84f11c5da60a5d08b282b98946ddc93a75
alexfortin <fortin.alex14@gmail.com>
2023-02-04 13:28:14 -0600

    refactored some code by making normal functions into arrow functions and passing arguments as an object instead of multiple variables

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,65 +1,72 @@
 var movieSelectionContainer = $("#movie-selection")
 var inputBox = $("#input-box")
 var moviesSelected = 0
-console.log(inputBox)
 
 //searches for the movie whenever you tyoe
-var searchMovie = function (event) {
+const searchMovie = event => {
     event.preventDefault(event);
     //input goes into "movie" variable
-    var movie = $(inputBox).val().trim()
-    console.log(movie)
+    const movie = $(inputBox).val().trim()
 
-    if (movie === "") {
-        var noMovie = document.querySelector("#no-movie")
+    if (!movie) {
+        const noMovie = document.querySelector("#no-movie")
         noMovie.classList.add("is-active")
 
-        $(noMovie).on("click", function () {
+        $(noMovie).on("click", () => {
             noMovie.classList.remove("is-active")
         })
+        return
     }
-    else {
-        callMovieInfo(movie)
-    }
+
+    callMovieInfo(movie)
 }
 
-var callMovieInfo = function (movie) {
-    //api url uses the movie as the query parameter to call whatever movie the user types
-    var apiUrl = "https://www.omdbapi.com?s=" + movie + "&t=movie&plot=&apikey=6f89013e"
+$("#search-button").on("click", searchMovie)
 
-    fetch(apiUrl).then(function (response) {
-        response.json().then(function (data) {
+const callMovieInfo = movie => {
+    //api url uses the movie as the query parameter to call whatever movie the user types
+    const apiUrl = "https://www.omdbapi.com?s=" + movie + "&t=movie&plot=&apikey=6f89013e"
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
             movieSelectionContainer.empty()
+
             console.log(data)
+
             if (data.Response == "False") {
-                var notValid = document.querySelector("#not-valid")
+                let notValid = document.querySelector("#not-valid")
                 notValid.classList.add("is-active")
 
-                $(notValid).on("click", function () {
+                $(notValid).on("click", () => {
                     notValid.classList.remove("is-active")
                 })
+                return
             }
-            else {
-                //for the lenght movie search results it will asign the values
-                for (var i = 0; i < data.Search.length; i++) {
-                    console.log(data)
-                    var movieTitle = data.Search[i].Title
-                    var movieYear = data.Search[i].Year
-                    var moviePoster = data.Search[i].Poster
-                    if (moviePoster === "N/A") {
-                        break
-                    }
-                    var imdbId = (data.Search[i].imdbID)
-                    //calls the plot function and stores the asigned values to be used later
-                    callPlot(imdbId, movieTitle, movieYear, moviePoster)
+            //for the lenght movie search results it will asign the values
+            for (let i = 0; i < data.Search.length; i++) {
+
+                const imdbId = data.Search[i].imdbID
+                const movieTitle = data.Search[i].Title
+                const movieYear = data.Search[i].Year
+                const moviePoster = data.Search[i].Poster
+                if (moviePoster === "N/A") {
+                    break
                 }
+                
+                const movieData = {
+                    imdbId,
+                    movieTitle,
+                    movieYear,
+                    moviePoster
+                }
+
+                //calls the plot function and stores the asigned values to be used later
+                callPlot(movieData)
             }
-
         })
-
-    })
-        .catch(function () {
-            var badInternet = document.querySelector("#bad-internet")
+        .catch(() => {
+            const badInternet = document.querySelector("#bad-internet")
             badInternet.classList.add("is-active")
 
             $(badInternet).on("click", function () {
@@ -68,48 +75,57 @@ var callMovieInfo = function (movie) {
         })
 }
 
-var callPlot = function (imdbId, movieTitle, movieYear, moviePoster) {
+const callPlot = movieData => {
+
+    const { imdbId, movieTitle } = movieData
 
     //uses the movie id and movie title as a parameter to call the plot
-    var plotUrl = "https://www.omdbapi.com?i=" + imdbId + "&t=" + movieTitle + "&plot=full&apikey=6f89013e"
+    const plotUrl = "https://www.omdbapi.com?i=" + imdbId + "&t=" + movieTitle + "&plot=full&apikey=6f89013e"
 
-    fetch(plotUrl).then(function (response) {
-        response.json().then(function (data) {
+    fetch(plotUrl)
+        .then(response => response.json())
+        .then(data => {
             //stored the genre and plot
-            var movieGenre = data.Genre
-            var moviePlot = data.Plot
+            const movieGenre = data.Genre
+            const moviePlot = data.Plot
+
+            movieData.movieGenre = movieGenre
+            movieData.moviePlot = moviePlot
+
             //calls function to display everything using all the variables as parameters
-            movieDropDown(movieTitle, movieYear, moviePoster, movieGenre, moviePlot)
+            movieDropDown(movieData)
         })
-    })
 }
 
-var movieDropDown = function (movieTitle, movieYear, moviePoster, movieGenre, moviePlot) {
+const movieDropDown = movieData => {
+
+    const { movieTitle, movieYear, moviePoster, movieGenre, moviePlot } = movieData
+
     //creates box to store all the elemnts
-    var movieCard = document.createElement("div")
+    const movieCard = document.createElement("div")
     movieCard.className = "display-movie-box card"
 
-    var inputContainer = document.querySelector(".input-container")
+    const inputContainer = document.querySelector(".input-container")
     inputContainer.classList.remove("starter-input-container")
 
     //card Image
-    var cardImage = document.createElement("div")
+    const cardImage = document.createElement("div")
     cardImage.className = "card-image"
-    var figureImage = document.createElement('figure')
+    const figureImage = document.createElement('figure')
     figureImage.className = "image is-3by4"
-    var posterImage = document.createElement("img")
+    const posterImage = document.createElement("img")
     posterImage.className = "form-movie-poster"
     posterImage.setAttribute("src", moviePoster)
 
     // card content 
-    var contentCard = document.createElement('div');
+    const contentCard = document.createElement('div');
     contentCard.className = 'card-content';
-    var conctent = document.createElement('div')
-    conctent.className = 'content'
-    var titleParagraph = document.createElement("p")
-    var yearParagraph = document.createElement("p")
-    var genreParagraph = document.createElement("p")
-    var plotParagraph = document.createElement("p")
+    const content = document.createElement('div')
+    content.className = 'content'
+    const titleParagraph = document.createElement("p")
+    const yearParagraph = document.createElement("p")
+    const genreParagraph = document.createElement("p")
+    const plotParagraph = document.createElement("p")
 
     titleParagraph.innerText = movieTitle
     titleParagraph.classList = "form-movie-title"
@@ -124,57 +140,49 @@ var movieDropDown = function (movieTitle, movieYear, moviePoster, movieGenre, mo
     $(figureImage).append(posterImage)
     $(cardImage).append(figureImage)
     $(movieCard).append(cardImage)
-    $(conctent).append(titleParagraph)
-    $(conctent).append(yearParagraph)
-    $(conctent).append(genreParagraph)
-    $(conctent).append(plotParagraph)
-    $(contentCard).append(conctent)
+    $(content).append(titleParagraph)
+    $(content).append(yearParagraph)
+    $(content).append(genreParagraph)
+    $(content).append(plotParagraph)
+    $(contentCard).append(content)
     $(movieCard).append(contentCard);
     $(movieSelectionContainer).append(movieCard)
 
-    //creates the elements where the values will be stored
-    var posterImage = document.createElement("img")
-    posterImage.setAttribute("src", moviePoster)
-
 }
-$("#search-button").on("click", searchMovie)
 
 //when you click on the list of movies it will display that movie and all of its information
-$(movieSelectionContainer).on("click", ".display-movie-box", function (event) {
-    console.dir(this)
-    
+$(movieSelectionContainer).on("click", ".display-movie-box", event => {
+    const movieSelected = event.target.closest(".card").children
 
     //calls the containers for each element from the html
-    var movieInformationContainer = document.querySelector("#movie-information")
+    const movieInformationContainer = document.querySelector("#movie-information")
     movieInformationContainer.className = "box";
-    var moviePoster = document.querySelector("#poster")
-    var movieTitle = document.querySelector("#movie-title")
-    var movieYear = document.querySelector("#movie-year")
-    var movieGenre = document.querySelector("#genre")
-    var moviePlot = document.querySelector("#plot")
+    const moviePoster = document.querySelector("#poster")
+    const movieTitle = document.querySelector("#movie-title")
+    const movieYear = document.querySelector("#movie-year")
+    const movieGenre = document.querySelector("#genre")
+    const moviePlot = document.querySelector("#plot")
 
     //creates the element where the values will be stored
-    var poster = document.createElement("img")
+    const poster = document.createElement("img")
     poster.className = "stylePoster"
-    var title = document.createElement("h2")
+    const title = document.createElement("h2")
     title.className = "styleTitle"
-    var year = document.createElement("p")
+    const year = document.createElement("p")
     year.className = "styleYear"
-    var genre = document.createElement("p")
+    const genre = document.createElement("p")
     genre.className = "styleGenre"
-    var plot = document.createElement("p")
+    const plot = document.createElement("p")
     plot.className = "stylePlot"
 
     //sets the values of the elements
-    poster.setAttribute("src", this.children[0].children[0].children[0].currentSrc)
-    title.innerText = this.children[1].children[0].children[0].innerText
-    year.innerText = this.children[1].children[0].children[1].innerText
-    genre.innerText = this.children[1].children[0].children[2].innerText
+    poster.setAttribute("src", movieSelected[0].children[0].children[0].currentSrc)
+    title.innerText = movieSelected[1].children[0].children[0].innerText
+    year.innerText = movieSelected[1].children[0].children[1].innerText
+    genre.innerText = movieSelected[1].children[0].children[2].innerText
     genre.classList.remove("hide")
-    plot.innerText = this.children[1].children[0].children[3].innerText
+    plot.innerText = movieSelected[1].children[0].children[3].innerText
     plot.classList.remove("hide")
-
-    console.dir(poster)
 
     //appends elements to their containers
     $(moviePoster).empty().append(poster)
@@ -189,22 +197,22 @@ $(movieSelectionContainer).on("click", ".display-movie-box", function (event) {
     }
 
     localStorage.setItem(moviesSelected, title.textContent)
-    
+
 })
 
 
-var storage = function () {
+const storage = function () {
 
     var inputContainer = document.querySelector(".input-container")
     inputContainer.classList.remove("starter-input-container")
     inputContainer.classList.add("storage-input-container")
 
-        var storedTitle = (localStorage.getItem(moviesSelected))
-        console.log(storedTitle)
-        callHistoryInfo(storedTitle)
+    var storedTitle = (localStorage.getItem(moviesSelected))
+    console.log(storedTitle)
+    callHistoryInfo(storedTitle)
 }
 
-var callHistoryInfo = function (storedTitle) {
+const callHistoryInfo = function (storedTitle) {
     var apiUrl = "https://www.omdbapi.com?s=" + storedTitle + "&t=movie&plot=&apikey=6f89013e"
 
     fetch(apiUrl).then(function (response) {
@@ -226,7 +234,7 @@ var callHistoryInfo = function (storedTitle) {
 }
 
 
-var callHistoryPlot = function (imdbId, movieTitle, movieYear, moviePoster) {
+const callHistoryPlot = function (imdbId, movieTitle, movieYear, moviePoster) {
     //uses the movie id and movie title as a parameter to call the plot
     var plotUrl = "https://www.omdbapi.com?i=" + imdbId + "&t=" + movieTitle + "&plot=full&apikey=6f89013e"
 
@@ -241,7 +249,7 @@ var callHistoryPlot = function (imdbId, movieTitle, movieYear, moviePoster) {
     })
 }
 
-var displayHistoryPlot = function (moTitle, moYear, moPoster, moGenre, moPlot) {
+const displayHistoryPlot = function (moTitle, moYear, moPoster, moGenre, moPlot) {
 
     console.dir(movieYear)
 
@@ -282,10 +290,10 @@ var displayHistoryPlot = function (moTitle, moYear, moPoster, moGenre, moPlot) {
     $(movieGenre).empty().append(genre)
     $(moviePlot).empty().append(plot)
 
-    
+
 }
 
-if(localStorage.getItem(moviesSelected)=== null) {
+if (localStorage.getItem(moviesSelected) === null) {
 }
 else {
     storage()
